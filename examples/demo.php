ae47eeececcc67ea488c187a82ec5914328b1685
runnerlee <runnerleer@gmail.com>
2016-10-24 10:41:20 +0000

    remove examples folder

--- examples/demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @author: RunnerLee
- * @email: runnerleer@gmail.com
- * @time: 2016-10-22 下午 6:07
- */
--- examples/shit.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * @author: RunnerLee
- * @email: runnerleer@gmail.com
- * @time: 2016-10-24 下午 6:35
- */
